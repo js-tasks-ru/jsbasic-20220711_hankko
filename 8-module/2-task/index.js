8665330edde04238c0b15b1d9ea1d87194a4c6ca
flipexe <104075088+flipexe@users.noreply.github.com>
2022-08-30 11:45:17 -0400

    update tasks

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -2,16 +2,16 @@ import createElement from '../../assets/lib/create-element.js';
 import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
-  #elem;
-  #productsFiltred;
-  #container;
-  #activeFilters = {};
+  elem;
+  productsFiltred;
+  container;
+  activeFilters = {};
   
   constructor(products) {
     this.products = products;
     this.filters = {};
-    this.#elem = this.render(this.products);
-    this.#productsFiltred = this.products;
+    this.elem = this.render(this.products);
+    this.productsFiltred = this.products;
   }
 
   ProductGridTemplate() {
@@ -23,7 +23,7 @@ export default class ProductGrid {
 
   render(products) {
     const productGridElem = createElement(this.ProductGridTemplate());
-    this.#container = productGridElem.querySelector('.products-grid__inner');
+    this.container = productGridElem.querySelector('.products-grid__inner');
     this.createProductElements(products);
     return productGridElem;
   }
@@ -31,35 +31,35 @@ export default class ProductGrid {
   createProductElements(products) {
     products.map((product) => {
       const productCardElem = new ProductCard(product);
-      this.#container.append(productCardElem.elem);
+      this.container.append(productCardElem.elem);
     });
   }
 
   updateFilter(filters) {
-    this.#productsFiltred = this.products;
-    const activeFilters = this.#activeFilters;
+    this.productsFiltred = this.products;
+    const activeFilters = this.activeFilters;
 
     Object.keys(filters).forEach(function eachKey(key) { 
       activeFilters[key] = filters[key];
     });
 
-    if (activeFilters.noNuts) {this.#productsFiltred = this.#productsFiltred.filter((product) => 
+    if (activeFilters.noNuts) {this.productsFiltred = this.productsFiltred.filter((product) => 
         product.nuts === false || product.nuts === undefined)}
 
-    if (activeFilters.vegeterianOnly) {this.#productsFiltred = this.#productsFiltred.filter((product) => 
+    if (activeFilters.vegeterianOnly) {this.productsFiltred = this.productsFiltred.filter((product) => 
         product.vegeterian === true)}
 
-    if (activeFilters.maxSpiciness) {this.#productsFiltred = this.#productsFiltred.filter((product) =>
+    if (activeFilters.maxSpiciness) {this.productsFiltred = this.productsFiltred.filter((product) =>
         product.spiciness <= activeFilters.maxSpiciness)}
 
-    if (activeFilters.category) {this.#productsFiltred = this.#productsFiltred.filter((product) => 
+    if (activeFilters.category) {this.productsFiltred = this.productsFiltred.filter((product) => 
         product.category === activeFilters.category)}
 
-    this.#container.innerHTML = '';
-    this.createProductElements(this.#productsFiltred);
+    this.container.innerHTML = '';
+    this.createProductElements(this.productsFiltred);
   }
 
   get elem() {
-    return this.#elem;
+    return this.elem;
   }
 }
--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,28 +6,56 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (product) {
+      const items = this.cartItems.find((item) => item.product.id === product.id);
+      if (items) {
+        this.cartItems.map((item) => {
+          if (item.product.id === items.product.id) {
+            item.count += 1;
+            this.cartItem = item;
+          }
+        });
+      } else {
+        this.cartItem = {
+          product: product,
+          count: 1
+        };
+        this.cartItems.push(this.cartItem);
+      }
+    } else {
+      return;
+    }
+    this.onProductUpdate(this.cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    this.cartItems.map((item => {
+      if (item.product.id === productId) {
+        item.count += amount;
+        this.cartItem = item;
+        if (item.count === 0) {
+          this.cartItems = this.cartItems.filter((item) => item.product.id !== productId);
+        }
+      }
+    }));
+    this.onProductUpdate(this.cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((totalItems, items) => totalItems + items.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce(
+      (totalItems, items) => totalItems + items.count * items.product.price, 0);
   }
-
+  
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
